9842053c52915abc76ccbbf12004dfde8f069a13
Jakub <stasiewski.jakub@gmail.com>
2023-07-10 16:19:07 +0200

    some style and scrolable with flatlist || TODO saving data

--- ToDoApp/App.js
+++ ToDoApp/App.js
@@ -1,59 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView } from 'react-native';
-import Task from './components/Task'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, KeyboardAvoidingView, SafeAreaView, FlatList } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   const [task, setTask] = useState('');
   const [tasksItems, setTasksItems] = useState([]);
 
   const handleAddTask = () => {
-    console.log(task)
     setTasksItems([...tasksItems, task]);
-    setTask(null)
-  }
+    setTask(null);
+  };
 
   const handleDelete = (index) => {
-    setTasksItems(tasksItems.filter((value, ind) => ind !== index))
-  }
+    setTasksItems(tasksItems.filter((value, ind) => ind !== index));
+  };
+
+  const renderItem = ({ item, index }) => (
+    <Task key={index} index={index} text={item} delete={handleDelete} />
+  );
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>ToDoApp</Text>
-        <View style={styles.items}>
-          {
-            tasksItems.map((item, index) => {
-              //console.log(`${item}  ${index}`)
-              return <Task key={index} index={index} text={item} delete={handleDelete} />
-            })
-          }
-        </View>
+        <FlatList
+          data={tasksItems}
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+          style={styles.items}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
-      <KeyboardAvoidingView>
-        <TextInput value={task} placeholder='write your tasks' onChangeText={text => setTask(text)} onSubmitEditing={() => handleAddTask()}></TextInput>
+      <KeyboardAvoidingView style={styles.inputArea}>
+        <TextInput
+          style={styles.input}
+          value={task}
+          placeholder="Write your tasks"
+          onChangeText={(text) => setTask(text)}
+          onSubmitEditing={() => handleAddTask()}
+        />
         <TouchableOpacity onPress={() => handleAddTask()}>
-          <View>
-            <Text>+</Text>
+          <View style={styles.addButton}>
+            <Text style={styles.buttonFont}>+</Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === 'android' ? 50 : 0,
   },
   tasksWrapper: {
-    paddingTop: 80,
+    flex: 1,
     paddingLeft: 20,
+    paddingRight: 20,
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
+    padding: 5,
+    paddingBottom: 15,
+    fontWeight: 'bold',
   },
-  items: {},
+  items: {
+    borderRadius: 10,
+  },
+  inputArea: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    height: 60,
+    borderTopWidth: 5,
+    borderTopColor: 'white'
+  },
+  input: {
+    flex: 1,
+    backgroundColor: '#00ccff',
+    padding: 10,
+    width: 'auto',
+  },
+  addButton: {
+    justifyContent: 'center',
+    backgroundColor: '#00ccff',
+    height: '100%',
+  },
+  buttonFont: {
+    fontSize: 30,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
 });
--- ToDoApp/components/Task.js
+++ ToDoApp/components/Task.js
@@ -1,19 +1,40 @@
 import React from 'react'
-import {View,Text,StyleSheet, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 const Task = (props) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <Text selectable={true} style={styles.taskText}>
         <Text>{props.text}</Text>
-        <TouchableOpacity onPress={()=>props.delete(props.index)}>
-            <Text>X</Text>
-        </TouchableOpacity>
+      </Text>
+      <TouchableOpacity onPress={() => props.delete(props.index)}>
+        <Text style={styles.deleteButton}>Delete</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    backgroundColor: '#33ccff',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  taskText: {
+    flex: 1,
+    paddingRight: 10,
+  },
+  deleteButton: {
+    height: 42,
+    padding: 10,
+    backgroundColor: '#99ccff',
+    borderRadius: 10,
+  }
 });
 
 export default Task;
